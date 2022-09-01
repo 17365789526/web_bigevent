de62e4a103b10ca26102786aa1bcc2627759a04e
mlb <1528238450@qq.com>
2022-09-01 09:39:02 +0800

    完成文章管理相关功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,11 +1,13 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给 Ajax 提供的配置对象
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  // options.url = 'http://ajax.frontend.itheima.net' + options.url
+  // 页面正常的 api 接口
   options.url = 'http://www.liulongbin.top:3007' + options.url
-  console.log(options.url)
-
+  // 文章页面 添加 删除 修改的接口
+  // options.url = 'http://api-breakingnews-web.itheima.net' + options.url
 
   // 统一为有权限的接口，设置 headers 请求头
   if (options.url.indexOf('/my/') !== -1) {
@@ -13,7 +15,6 @@ $.ajaxPrefilter(function (options) {
       Authorization: localStorage.getItem('token') || ''
     }
   }
-
 
   // 全局统一挂载 complete 回调函数
   options.complete = function (res) {
@@ -24,7 +25,7 @@ $.ajaxPrefilter(function (options) {
       // 1. 强制清空 token
       localStorage.removeItem('token')
       // 2. 强制跳转到登录页面
-      location.href = './login.html'
+      location.href = '/login.html'
     }
   }
 })
